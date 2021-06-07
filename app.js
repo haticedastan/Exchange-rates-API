ec5ef69a5c857944a8cd263f660028e04f874de0
Hatice <40773971+haticedastan@users.noreply.github.com>
2021-06-07 15:57:28 +0300

    UI

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const amountElement=document.querySelector("#amount");
 const firstSelect= document.querySelector("#firstCurrency");
 const secondSelect= document.querySelector("#secondCurrency");
 const currency = new Currency("USD","TRY")
+const ui=new UI(firstSelect,secondSelect)
 eventListener();
 
 function eventListener(){
     amountElement.addEventListener("input",exchangeCurrency);
     firstSelect.onchange = function(){
         currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent);
-
+        ui.changeFirst();
     };
     
     secondSelect.onchange = function(){
         currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent);
+        ui.changeSecond();
 
 
     };
@@ -23,6 +25,12 @@ function exchangeCurrency(){
 
     currency.changeAmount(amountElement.value);
 
-    currency.exchange();
-    
+    currency.exchange()
+
+    .then(result => {
+        ui.displayResult(result);
+    })
+
+    .catch((err) => console.log(err));
+
 }
--- currency.js
+++ currency.js
@@ -2,28 +2,30 @@ class Currency{
     constructor(firstCurrency,secondCurrency){
         this.firstCurrency=firstCurrency;
         this.secondCurrency=secondCurrency;
-        this.url = "http://api.exchangeratesapi.io/v1/latest?access_key=YOUkey&format=1";
+        this.url = "http://api.exchangeratesapi.io/v1/latest?access_key=YOUKEY&format=1";
         this.amount = null ; 
     }
 
     exchange(){
-        fetch(this.url + this.firstCurrency)
+        return new Promise((resolve,reject)=>{
+            fetch(this.url + this.firstCurrency)
 
-        .then(response => response.json())
-
-        .then(data => {
-
-            const parity = data.rates[this.secondCurrency];
-            const amount2=Number(this.amount);
-            let total= parity*amount2;
-            console.log(total);
-
-          })
-
-          .catch((err) => {
-              console.err(err);
-          });
-
+            .then(response => response.json())
+    
+            .then(data => {
+    
+                // const parity = data.rates[this.secondCurrency];
+                const parity = data["rates"][this.secondCurrency]
+                const amount2=Number(this.amount);
+                let total= parity*amount2;
+                resolve(total);
+    
+    
+              })
+    
+              .catch((err) => reject(err));
+            
+        });
         }
 
         changeAmount(amount){
